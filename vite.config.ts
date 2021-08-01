0a69e9edef2b45c0ef951c104239e5e2396d6b4b
tascript <wataruscript8611@gmail.com>
2021-08-01 21:51:52 +0900

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
 
-const config = defineConfig({})
+const config = defineConfig({
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess()
+    })
+  ]
+})
 
 export default config
