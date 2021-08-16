aeaddbae4c799e9f04a72730d44e4809f41947c3
tascript <wataruscript8611@gmail.com>
2021-08-17 00:54:08 +0900

    use store directive

--- src/component/Counter.svelte
+++ src/component/Counter.svelte
@@ -1,19 +1,12 @@
 <script lang='ts'>
 import { total } from '../store'
-import type { Unsubscriber } from 'svelte/store'
-import { onDestroy } from 'svelte'
 const countUp = () => {
   total.update(v => v + 1)
 }
-let current: number
 
-const unsubscriber: Unsubscriber = total.subscribe(t => {
-  current = t
-  })
-onDestroy(unsubscriber) 
 </script>
 
-<p>current count is { current }</p>
+<p>current count is {$total}</p>
 
 <button on:click={countUp}>
   count up
